253d5ad73bd96496acce723977533f7d2ddd4d5f
Renan Dresch <renan@medroom.com.br>
2020-08-19 21:32:32 -0300

    Version 1.0

---
+++ Assets/Example/Scripts/ColorSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorSwitcher : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    private Image image = default;
+
+    #endregion
+
+    #region Public Methods
+
+    public void SetRedColor()
+    {
+        image.color = Color.red ;
+    }
+
+    public void SetBlueColor()
+    {
+        image.color = Color.blue;
+    }
+
+    public void SetGreenColor()
+    {
+        image.color = Color.green;
+    }
+
+    #endregion
+}
---
+++ Assets/Example/Scripts/VirtualPointerMouseController.cs
@@ -0,0 +1,41 @@
+﻿using Gaze.VirtualPointer;
+using UnityEngine;
+
+public class VirtualPointerMouseController : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    private VirtualPointer pointer = default;
+
+    private Vector3 lastMousePosition;
+
+    #endregion
+
+    #region Private Methods
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        var mouseDelta = Input.mousePosition - lastMousePosition;
+        if(mouseDelta.magnitude != 0)
+        {
+            pointer.SetPointerPosition(Input.mousePosition);
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            pointer.PointerDown = true;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            pointer.PointerDown = false;
+        }
+        lastMousePosition = Input.mousePosition;
+    }
+
+    #endregion
+}
